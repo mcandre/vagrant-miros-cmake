5a8bdaa140bd3e23123fdc526214644c80488152
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2019-02-03 13:01:39 -0600

    work around limitations in mirbsd supported errno values

--- i386/bootstrap.sh
+++ i386/bootstrap.sh
@@ -6,6 +6,7 @@ sudo env PKG_PATH=https://www.mirbsd.org/MirOS/v10/Pkgs/i386 /usr/mpkg/sbin/pkg_
         patch -p0 </tmp/core.c.patch &&
         patch -p0 </tmp/fs.c.patch &&
         patch -p0 </tmp/getaddrinfo.c.patch &&
+        patch -p0 </tmp/getnameinfo.c.patch &&
         find . -name '*.orig' -print -exec rm -rf \"{}\" \; &&
         env CC=mgcc CXX=g++ -D_POSIX_SOURCE=1 ./configure --prefix=/usr && \
         make && \
