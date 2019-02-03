88e2cff95e3d7733c9d6edff6950e62c62c71d0f
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2019-02-03 12:51:23 -0600

    work around limitations in mirbsd supported errno values

--- i386/bootstrap.sh
+++ i386/bootstrap.sh
@@ -5,8 +5,9 @@ sudo env PKG_PATH=https://www.mirbsd.org/MirOS/v10/Pkgs/i386 /usr/mpkg/sbin/pkg_
     sh -c "cd cmake-3.9.0 && \
         patch -p0 </tmp/core.c.patch &&
         patch -p0 </tmp/fs.c.patch &&
+        patch -p0 </tmp/getaddrinfo.c.patch &&
         find . -name '*.orig' -print -exec rm -rf \"{}\" \; &&
-        env CC=mgcc CXX=g++ -D_POSIX_SOURCE=1 CFLAGS=-save-temps CXXFLAGS=-save-temps ./configure --prefix=/usr && \
+        env CC=mgcc CXX=g++ -D_POSIX_SOURCE=1 ./configure --prefix=/usr && \
         make && \
         sudo make install" &&
     rm -rf cmake-3.9.0 &&
